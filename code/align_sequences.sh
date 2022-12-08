02e0cb6f0c0660cfa979266ee0fa03fd9b4a2408
adimascf <cahyaningdimas@gmail.com>
2022-12-08 19:26:29 +0700

    Modify sequence headers to remove spaces, closes #15

--- code/align_sequences.sh
+++ code/align_sequences.sh
@@ -8,5 +8,15 @@
 # We need to include flip=T to make sure all sequences are pointed
 # in the same direction
 
+sed "s/ /_/g" data/raw/rrnDB-5.8_16S_rRNA.fasta > data/raw/rrnDB-5.8_16S_rRNA.temp.fasta
 
-./code/mothur/mothur '#align.seqs(fasta=data/raw/rrnDB-5.8_16S_rRNA.fasta, reference=data/references/silva_seed/silva.seed_v138_1.align, flip=T)'
+./code/mothur/mothur '#align.seqs(fasta=data/raw/rrnDB-5.8_16S_rRNA.temp.fasta, reference=data/references/silva_seed/silva.seed_v138_1.align, flip=T)'
+
+if [[ $ -eq 0 ]]
+then
+    mv data/raw/rrnDB-5.8_16S_rRNA.temp.align data/raw/rrnDB-5.8_16S_rRNA.align
+    rm data/raw/rrnDB-5.8_16S_rRNA.temp.fasta
+else
+    echo "FAIL: mothur failed to align sequences"
+    exit 1
+fi
