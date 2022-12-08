f5e14f0d909e6863ff4ec6a9c9ff3c322c322576
adimascf <cahyaningdimas@gmail.com>
2022-12-08 20:41:07 +0700

    Correct the if statement

--- code/align_sequences.sh
+++ code/align_sequences.sh
@@ -12,7 +12,7 @@ sed "s/ /_/g" data/raw/rrnDB-5.8_16S_rRNA.fasta > data/raw/rrnDB-5.8_16S_rRNA.te
 
 ./code/mothur/mothur '#align.seqs(fasta=data/raw/rrnDB-5.8_16S_rRNA.temp.fasta, reference=data/references/silva_seed/silva.seed_v138_1.align, flip=T)'
 
-if [[ $ -eq 0 ]]
+if [[ $? -eq 0 ]]
 then
     mv data/raw/rrnDB-5.8_16S_rRNA.temp.align data/raw/rrnDB-5.8_16S_rRNA.align
     rm data/raw/rrnDB-5.8_16S_rRNA.temp.fasta
