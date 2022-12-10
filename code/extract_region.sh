53eb780b39d72e8ce91746880c76fd2656619185
adimascf <cahyaningdimas@gmail.com>
2022-12-10 19:25:46 +0700

    Replace leading periods with hyphens, closes #18

--- code/extract_region.sh
+++ code/extract_region.sh
@@ -45,18 +45,31 @@ mkdir -p $path
 code/mothur/mothur "#pcr.seqs(fasta=data/raw/rrnDB-5.8_16S_rRNA.align, start=$start, end=$end, outputdir=$path);
 filter.seqs(vertical=TRUE)"
 
+# grep -v "^>" $path/rrnDB-5.8_16S_rRNA.pcr.filter.fasta | grep "\."
+
 # If mothur executed sucessfully, then touch the files thaty might not have been
 # generated in pcr.seqs because the sequence spanned the desired region's 
 # coordinates
 if [[ $? -eq 0 ]]
 then
-    sed "s/^\.+/-/" $path/rrnDB-5.8_16S_rRNA.pcr.filter.fasta > $path/rrnDB-5.8_16S_rRNA.pcr.filter.test.fasta
+    sed '/^[^>]/ s/\./-/g' $path/rrnDB-5.8_16S_rRNA.pcr.filter.fasta > $path/rrnDB-5.8_16S_rRNA.pcr.filter.test.fasta
+    
+
     touch $path/rrnDB-5.8_16S_rRNA.bad.accnos
     touch $path/rrnDB-5.8_16S_rRNA.scrap.pcr.align
 else
     echo "FAIL: mothur run into a problem"
     exit 1
 fi
+
+TEST=`grep -v "^>" $path/rrnDB-5.8_16S_rRNA.pcr.filter.test.fasta | grep -c "\."`
+
+if [[ $TEST -ne 0 ]]
+then
+    echo "FAIL: sequences contained periods."
+    exit 1
+fi
+
 
 # clean up the file names
 mv $path/rrnDB-5.8_16S_rRNA.pcr.filter.test.fasta $target
