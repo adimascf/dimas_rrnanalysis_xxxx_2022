d64bd5f58fcacbd17aea17fd37af4b3657e99a0c
adimascf <cahyaningdimas@gmail.com>
2022-12-11 00:44:30 +0700

    Edit pipeline to acomodate rrnDB version 5.6

--- code/extract_region.sh
+++ code/extract_region.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Author: adimascf
-# Dependency: data/raw/rrnDB-5.8_16S_rRNA.fasta
+# Dependency: data/raw/rrnDB-5.6_16S_rRNA.fasta
 # Input: the target anem, which is really the output
 #       data/<region>/rrnDB.align
 
@@ -42,27 +42,27 @@ fi
 
 mkdir -p $path
 
-code/mothur/mothur "#pcr.seqs(fasta=data/raw/rrnDB-5.8_16S_rRNA.align, start=$start, end=$end, outputdir=$path);
+code/mothur/mothur "#pcr.seqs(fasta=data/raw/rrnDB-5.6_16S_rRNA.align, start=$start, end=$end, outputdir=$path);
 filter.seqs(vertical=TRUE)"
 
-# grep -v "^>" $path/rrnDB-5.8_16S_rRNA.pcr.filter.fasta | grep "\."
+# grep -v "^>" $path/rrnDB-5.6_16S_rRNA.pcr.filter.fasta | grep "\."
 
 # If mothur executed sucessfully, then touch the files thaty might not have been
 # generated in pcr.seqs because the sequence spanned the desired region's 
 # coordinates
 if [[ $? -eq 0 ]]
 then
-    sed '/^[^>]/ s/\./-/g' $path/rrnDB-5.8_16S_rRNA.pcr.filter.fasta > $path/rrnDB-5.8_16S_rRNA.pcr.filter.test.fasta
+    sed '/^[^>]/ s/\./-/g' $path/rrnDB-5.6_16S_rRNA.pcr.filter.fasta > $path/rrnDB-5.6_16S_rRNA.pcr.filter.test.fasta
     
 
-    touch $path/rrnDB-5.8_16S_rRNA.bad.accnos
-    touch $path/rrnDB-5.8_16S_rRNA.scrap.pcr.align
+    touch $path/rrnDB-5.6_16S_rRNA.bad.accnos
+    touch $path/rrnDB-5.6_16S_rRNA.scrap.pcr.align
 else
     echo "FAIL: mothur run into a problem"
     exit 1
 fi
 
-TEST=`grep -v "^>" $path/rrnDB-5.8_16S_rRNA.pcr.filter.test.fasta | grep -c "\."`
+TEST=`grep -v "^>" $path/rrnDB-5.6_16S_rRNA.pcr.filter.test.fasta | grep -c "\."`
 
 if [[ $TEST -ne 0 ]]
 then
@@ -72,14 +72,14 @@ fi
 
 
 # clean up the file names
-mv $path/rrnDB-5.8_16S_rRNA.pcr.filter.test.fasta $target
-mv $path/rrnDB-5.8_16S_rRNA.bad.accnos $path/rrnDB.bad.accnos
+mv $path/rrnDB-5.6_16S_rRNA.pcr.filter.test.fasta $target
+mv $path/rrnDB-5.6_16S_rRNA.bad.accnos $path/rrnDB.bad.accnos
 
 # garbage collection
-rm $path/rrnDB-5.8_16S_rRNA.pcr.align
-rm $path/rrnDB-5.8_16S_rRNA.scrap.pcr.align
+rm $path/rrnDB-5.6_16S_rRNA.pcr.align
+rm $path/rrnDB-5.6_16S_rRNA.scrap.pcr.align
 rm $path/rrnDB-5.filter
-rm $path/rrnDB-5.8_16S_rRNA.pcr.filter.fasta
+rm $path/rrnDB-5.6_16S_rRNA.pcr.filter.fasta
 
 
 
--- code/get_rrndb_files.sh
+++ code/get_rrndb_files.sh
@@ -8,6 +8,7 @@ target=$1
 filename=`echo $target | sed "s/.*\///"`
 path=`echo $target | sed -E "s/(.*\/).*/\1/"`
 
+https://rrndb.umms.med.umich.edu/static/download/rrnDB-5.6_16S_rRNA.fasta.zip
 wget -P "$path" -nc https://rrndb.umms.med.umich.edu/static/download/"$filename".zip
 unzip -n -d "$path" "$target".zip
 
